5461bcc6eb02d6e5ee90dde27013c0ec83f75155
raiden igarashi <gamerraiden6@gmail.com>
2025-06-08 15:30:44 -0300

    Troca de nomes
    Coloquei backend em "server" só coisa de estrutura msm

--- backend/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-async function connectDB() {
-  await mongoose.connect('mongodb://localhost:27017/web-app');
-  console.log('✅ MongoDB conectado');
-}
-
-module.exports = connectDB;
