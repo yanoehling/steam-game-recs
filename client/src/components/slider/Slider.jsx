189f1915f76c74d0ec761106240c546f2b795daa
raiden igarashi <gamerraiden6@gmail.com>
2025-06-25 13:57:09 -0300

    Update Slider.jsx

--- client/src/components/slider/Slider.jsx
+++ client/src/components/slider/Slider.jsx
@@ -1,61 +1,78 @@
 import '../../style/main.css';
-import undertale_banner from '../../pages/home/Undertale_banner.png';
-import hollowKnight_banner from '../../pages/home/Hollow_Knight_Banner.png';
-import lastOfUs_banner from '../../pages/home/The_Last_Of_Us_Banner.png';
 import { useNavigate } from 'react-router-dom';
-let qt = 2;
+import { useEffect } from 'react';
 let count = 0;
 let intervalId;
 
-function slider_loop(){
-  clearInterval(intervalId)
-  intervalId = setInterval(()=>{
+
+function Showcase_imgs(showcase_game_info, is_anchours){
+  return (
+    <>
+      {showcase_game_info.map((showcase)=> (
+        is_anchours ? (<a href={showcase.href}>
+          <img src={showcase.img} alt={showcase.name}/>
+        </a>) : (<img src={showcase.img} alt={showcase.name}/>)
+      ))}
+    </>
+  );
+}
+function Slider({showcase_game_info, is_anchours, qt}){
+
+  useEffect(() => {
+    slider_loop(qt);
+    return () => clearInterval(intervalId);
+  }, [qt]);
+
+  useEffect(() => {
+    slider_loop();
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const slider_loop = () => {
+    clearInterval(intervalId)
+    intervalId = setInterval(()=>{
+      count++
+      if(count > qt){
+          count = 0
+      }
+      const slider = document.getElementById('slider')
+      let marginpercent = 200*count;
+      slider.style.marginLeft = `-${marginpercent}%`;
+      slider.style.transition = "1s";
+  }, 3000)
+  }
+
+  const slider_pass_next = () => {
+    clearInterval(intervalId)
     count++
-    if(count > qt){
-        count = 0
+    if (count > qt){
+      count = 0
     }
     const slider = document.getElementById('slider')
     let marginpercent = 200*count;
     slider.style.marginLeft = `-${marginpercent}%`;
     slider.style.transition = "1s";
-}, 3000)
-}
-
-function slider_pass_next(){
-  clearInterval(intervalId)
-  count++
-  if (count > qt){
-    count = 0
+    setTimeout(slider_loop, 5000)
   }
-  const slider = document.getElementById('slider')
-  let marginpercent = 200*count;
-  slider.style.marginLeft = `-${marginpercent}%`;
-  slider.style.transition = "1s";
-  setTimeout(slider_loop, 5000)
-}
 
-function slider_pass_before(){
-  clearInterval(intervalId)
-  count--
-  if (count < 0){
-    count = qt
+  const slider_pass_before = () => {
+    clearInterval(intervalId)
+    count--
+    if (count < 0){
+      count = qt
+    }
+    const slider = document.getElementById('slider')
+    let marginpercent = 200*count;
+    slider.style.marginLeft = `-${marginpercent}%`;
+    slider.style.transition = "1s";
+    setTimeout(slider_loop, 5000)
   }
-  const slider = document.getElementById('slider')
-  let marginpercent = 200*count;
-  slider.style.marginLeft = `-${marginpercent}%`;
-  slider.style.transition = "1s";
-  setTimeout(slider_loop, 5000)
-}
-
-function Slider(){
-  slider_loop()
+  
   return(
     <>
       <section className='showcase-game'>
         <div className="showcase-div" id="slider">
-          <a href="/game"><img src={undertale_banner} alt="Undertale Banner"/></a>
-          <a href="#"><img src={hollowKnight_banner} alt="Hollow Knight Banner"/></a>
-          <a href="#"><img src={lastOfUs_banner} alt="The Last Of Us Banner"/></a>
+          {Showcase_imgs(showcase_game_info, is_anchours)}
         </div>
         <button className="btn back-btn" onClick={slider_pass_before}></button>
         <button className="btn next-btn" onClick={slider_pass_next}></button>
